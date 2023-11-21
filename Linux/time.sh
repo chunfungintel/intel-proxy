2f0ada03ebb86dfb34e7d2badb3ff23f5274a4a2
Leow Chun Fung <chun.fung.leow@intel.com>
2023-11-21 12:29:47 +0800

    Daemon reload

--- Linux/time.sh
+++ Linux/time.sh
@@ -2,6 +2,7 @@
 
 echo "NTP=ntp-fm11d.cps.intel.com 10.128.4.200 10.128.4.201 corp.intel.com" | sudo tee -a /etc/systemd/timesyncd.conf
 sudo timedatectl set-timezone Asia/Singapore
+sudo systemctl daemon-reload
 sudo systemctl enable --now systemd-timesyncd
 sudo systemctl restart systemd-timesyncd
 
