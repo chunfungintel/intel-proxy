d0ba5ddad656ec3f5b8e7f3975a3310a51c4fd10
Time Leow <chfung2020@yahoo.com>
2023-12-22 12:14:22 +0800

    Default not change to systemd cgroup

--- Containerd/config.sh
+++ Containerd/config.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir -p /etc/containerd
 sudo containerd config default |& sudo tee /etc/containerd/config.toml
-sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+#sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
 sudo systemctl daemon-reload && sudo systemctl restart containerd.service
 
